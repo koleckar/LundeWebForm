5c7290f37a2a09d1d5c071a73f72118c917963f7
David Kolečkář <koleckar@protonmail.com>
2023-01-04 12:57:57 +0100

    Created .jar

--- src/main/java/com/dk/lundegaard/LundegaardMockApp/utils/Constants.java
+++ src/main/java/com/dk/lundegaard/LundegaardMockApp/utils/Constants.java
@@ -1,12 +1,10 @@
 package com.dk.lundegaard.LundegaardMockApp.utils;
 
 public class Constants {
-
     public static final int POLICY_NUMBER_LEN = 6;
     public static final int MIN_NAME_LEN = 2;
     public static final int MIN_SURNAME_LEN = 2;
     public static final int MIN_REQUEST_MESSAGE_LEN = 10;
-
     public static final int MAX_NAME_LEN = 20;
     public static final int MAX_SURNAME_LEN = 20;
     public static final int MAX_REQUEST_MESSAGE_LEN = 5000;
